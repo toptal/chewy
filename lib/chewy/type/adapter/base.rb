4cea6987642898c2d9dbe10d3d14d3a0d2bbe66a
pyromaniac <kinwizard@gmail.com>
2013-12-12 22:46:14 +0700

    Travis config improvements

--- lib/chewy/type/adapter/base.rb
+++ lib/chewy/type/adapter/base.rb
@@ -1,13 +1,21 @@
 module Chewy
   module Type
     module Adapter
+      # Basic adapter class. Contains interface, need to implement to add any classes support
       class Base
         BATCH_SIZE = 1000
 
+        # Camelcased name, used as type class constant name.
+        # For returned value 'Product' will be generated class name `ProductsIndex::Product`
+        #
         def name
           raise NotImplementedError
         end
 
+        # Underscored type name, user for elasticsearch type creation
+        # and for type class access with ProductsIndex.types hash or method.
+        # `ProductsIndex.types['product']` or `ProductsIndex.product`
+        #
         def type_name
           raise NotImplementedError
         end
@@ -21,6 +29,12 @@ module Chewy
           raise NotImplementedError
         end
 
+        # Returns array of loaded objects for passed objects array. If some object
+        # was not loaded, it returns `nil` in the place of this object
+        #
+        #   load(double(id: 1), double(id: 2), double(id: 3)) #=>
+        #     # [<Product id: 1>, nil, <Product id: 3>], assuming, #2 was not found
+        #
         def load *args
           raise NotImplementedError
         end

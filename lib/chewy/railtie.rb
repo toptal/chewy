2b5a84ac5a2b925164a7d0e8b176130760a53ee4
pyromaniac <kinwizard@gmail.com>
2014-01-23 22:08:28 +0700

    Added beautiful rails logging

--- lib/chewy/railtie.rb
+++ lib/chewy/railtie.rb
@@ -7,5 +7,17 @@ module Chewy
     initializer 'chewy.add_app_chewy_path' do |app|
       app.config.paths.add 'app/chewy'
     end
+
+    initializer 'chewy.add_requests_logging' do |app|
+      ActiveSupport::Notifications.subscribe('import_objects.chewy') do |name, start, finish, id, payload|
+        duration = ((finish - start).to_f * 10000).round / 10.0
+        Rails.logger.debug("  \e[1m\e[33m#{payload[:type]} Import (#{duration}ms)\e[0m #{payload[:import]}")
+      end
+
+      ActiveSupport::Notifications.subscribe('search_query.chewy') do |name, start, finish, id, payload|
+        duration = ((finish - start).to_f * 10000).round / 10.0
+        Rails.logger.debug("  \e[1m\e[33m#{payload[:index]} Search (#{duration}ms)\e[0m #{payload[:request]}")
+      end
+    end
   end
 end

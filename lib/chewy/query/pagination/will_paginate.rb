9fd6661cea8cd4732044998abbc078f49442caa2
José Coelho <contato@josecoelho.com>
2014-08-12 11:04:43 -0300

    cleaning the code

--- lib/chewy/query/pagination/will_paginate.rb
+++ lib/chewy/query/pagination/will_paginate.rb
@@ -2,34 +2,22 @@ module Chewy
   class Query
     module Pagination
       module WillPaginate
+        extend ActiveSupport::Concern
         include ::WillPaginate::CollectionMethods
+
+        included do
+          alias_method :total_entries, :total_count
+        end
+
+        attr_reader :current_page, :per_page
 
         def paginate(options = {})
           @current_page = ::WillPaginate::PageNumber(options[:page] || @current_page || 1)
           @page_multiplier = @current_page - 1
-
-          pp = (options[:per_page] || per_page || ::WillPaginate.per_page).to_i
+          @per_page = (options[:per_page] || @per_page || ::WillPaginate.per_page).to_i
 
           #call Chewy::Query methods to limit results
-          per_page(pp)
-          offset( @page_multiplier * pp )
-        end
-
-        def current_page(value=:non_given)
-          if value == :non_given
-            @current_page
-          else
-            @current_page = value
-          end
-        end
-
-        def per_page(value = :non_given)
-          if value == :non_given
-            @per_page
-          else
-            @per_page = value
-            limit(value)
-          end
+          limit(@per_page).offset( @page_multiplier * @per_page )
         end
 
         def page(page)
@@ -37,11 +25,7 @@ module Chewy
         end
 
         def total_pages
-          (total_entries / per_page.to_f).ceil
-        end
-
-        def total_entries
-          @total_entries ||= self.total_count
+          (total_entries / @per_page.to_f).ceil
         end
 
       end

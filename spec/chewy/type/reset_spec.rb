5a43c3e8ce642d98c92df2814998ab46a48f08a7
Jon Ford <jondavidford@gmail.com>
2014-08-20 16:00:34 -0500

    delete_by_query: add test for Chewy::Type::Reset.reset

---
+++ spec/chewy/type/reset_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Chewy::Type::Reset do
+  before { Chewy.client.indices.delete index: '*' }
+
+  before do
+    stub_model(:city)
+  end
+
+  before do
+    stub_index(:cities) do
+      define_type City do
+        field :name
+      end
+    end
+  end
+
+  let!(:dummy_cities) { 3.times.map { |i| City.create(name: "name#{i}") } }
+  let(:city) { CitiesIndex::City }
+
+  before do
+    city.import
+  end
+
+  describe '.reset' do
+    specify { expect { city.reset }.to update_index(city) }
+  end
+end

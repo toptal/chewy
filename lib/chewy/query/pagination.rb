cd4ec22c51d5d18b680a991633e14e98aa5c2df3
Mark Murphy <murphy.mark@live.ca>
2014-10-24 15:21:02 -0300

    Add "timeout" and "timed_out" methods.

--- lib/chewy/query/pagination.rb
+++ lib/chewy/query/pagination.rb
@@ -5,9 +5,11 @@ module Chewy
       #
       #   PlacesIndex.query(...).filter(...).total_count
       #
-      def total_count
+      def total
         _response['hits'].try(:[], 'total') || 0
       end
+
+      alias_method :total_count, :total
     end
   end
 end

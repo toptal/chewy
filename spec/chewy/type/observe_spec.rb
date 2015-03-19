db2141f2552d470cd291f04f1d6bfd54921040ea
pyromaniac <kinwizard@gmail.com>
2015-03-19 07:22:13 +0300

    Revert "Revert "use_after_commit_callbacks configuration option""
    This reverts commit c3f624030630999ce306e3187b1235b2b8eb7ff9.

--- spec/chewy/type/observe_spec.rb
+++ spec/chewy/type/observe_spec.rb
@@ -49,5 +49,35 @@ describe Chewy::Type::Import do
     specify { expect { city.save! }.to update_index('countries#country').and_reindex(city.country) }
     specify { expect { country.save! }.to update_index('cities#city').and_reindex(country.cities) }
     specify { expect { country.save! }.to update_index('countries#country').and_reindex(country) }
+
+  end
+
+  context 'transactions', :active_record do
+    context do
+      before { stub_model(:city) { update_index 'cities#city', :self } }
+      before { stub_index(:cities) { define_type City } }
+
+      specify do
+        Chewy.strategy(:urgent) do
+          ActiveRecord::Base.transaction do
+            expect { City.create! }.not_to update_index('cities#city')
+          end
+        end
+      end
+    end
+
+    context do
+      before { allow(Chewy).to receive_messages(use_after_commit_callbacks: false) }
+      before { stub_model(:city) { update_index 'cities#city', :self } }
+      before { stub_index(:cities) { define_type City } }
+
+      specify do
+        Chewy.strategy(:urgent) do
+          ActiveRecord::Base.transaction do
+            expect { City.create! }.to update_index('cities#city')
+          end
+        end
+      end
+    end
   end
 end

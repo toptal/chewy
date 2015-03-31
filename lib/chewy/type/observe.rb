02422f0188a4d27daa805723ddcb7057681c6726
pyromaniac <kinwizard@gmail.com>
2015-03-31 11:04:48 +0300

    Dry observers

--- lib/chewy/type/observe.rb
+++ lib/chewy/type/observe.rb
@@ -3,52 +3,41 @@ module Chewy
     module Observe
       extend ActiveSupport::Concern
 
-      module MongoidMethods
-        def update_index(type_name, *args, &block)
-          options = args.extract_options!
-          method = args.first
+      def self.update_proc(type_name, *args, &block)
+        options = args.extract_options!
+        method = args.first
 
-          update = Proc.new do
-            backreference = if method && method.to_s == 'self'
-              self
-            elsif method
-              send(method)
-            else
-              instance_eval(&block)
-            end
-
-            Chewy.derive_type(type_name).update_index(backreference, options)
+        Proc.new do
+          backreference = if method && method.to_s == 'self'
+            self
+          elsif method
+            send(method)
+          else
+            instance_eval(&block)
           end
 
-          after_save &update
-          after_destroy &update
+          Chewy.derive_type(type_name).update_index(backreference, options)
+        end
+      end
+
+      module MongoidMethods
+        def update_index(type_name, *args, &block)
+          update_proc = Observe.update_proc(type_name, *args, &block)
+
+          after_save &update_proc
+          after_destroy &update_proc
         end
       end
 
       module ActiveRecordMethods
         def update_index(type_name, *args, &block)
-          options = args.extract_options!
-          method = args.first
-
-          update = Proc.new do
-            # clear_association_cache if Chewy.strategy.current.name == :urgent
-
-            backreference = if method && method.to_s == 'self'
-              self
-            elsif method
-              send(method)
-            else
-              instance_eval(&block)
-            end
-
-            Chewy.derive_type(type_name).update_index(backreference, options)
-          end
+          update_proc = Observe.update_proc(type_name, *args, &block)
 
           if Chewy.use_after_commit_callbacks
-            after_commit &update
+            after_commit &update_proc
           else
-            after_save &update
-            after_destroy &update
+            after_save &update_proc
+            after_destroy &update_proc
           end
         end
       end

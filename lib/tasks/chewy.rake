559602d9e32fe6b4793093158f003a2872adafbb
Courtland Caldwell <courtland@mattermark.com>
2015-07-02 04:01:24 +0000

    Use singleton module RakeHelper instead of using include

--- lib/tasks/chewy.rake
+++ lib/tasks/chewy.rake
@@ -1,31 +1,30 @@
 require 'tasks/rake_helper'
 namespace :chewy do
-  include RakeHelper
   desc 'Destroy, recreate and import data to specified index'
   task :reset, [:index] => :environment do |task, args|
-    subscribe_task_stats!
-    args[:index].present? ? reset_index(args[:index]) : reset_all
+    RakeHelper.subscribe_task_stats!
+    args[:index].present? ? RakeHelper.reset_index(args[:index]) : RakeHelper.reset_all
   end
 
   namespace :reset do
     desc 'Destroy, recreate and import data for all found indexes'
     task all: :environment do
-      subscribe_task_stats!
-      reset_all
+      RakeHelper.subscribe_task_stats!
+      RakeHelper.reset_all
     end
   end
 
   desc 'Updates data specified index'
   task :update, [:index] => :environment do |task, args|
-    subscribe_task_stats!
-    args[:index].present? ? update_index(args[:index]) : update_all
+    RakeHelper.subscribe_task_stats!
+    args[:index].present? ? RakeHelper.update_index(args[:index]) : RakeHelper.update_all
   end
 
   namespace :update do
     desc 'Updates data for all found indexes'
     task all: :environment do
-      subscribe_task_stats!
-      update_all
+      RakeHelper.subscribe_task_stats!
+      RakeHelper.update_all
     end
   end
 end
--- lib/tasks/rake_helper.rb
+++ lib/tasks/rake_helper.rb
@@ -1,57 +1,59 @@
 module RakeHelper
-  def subscribe_task_stats!
-    ActiveSupport::Notifications.subscribe('import_objects.chewy') do |name, start, finish, id, payload|
-      duration = (finish - start).round(2)
-      puts "  Imported #{payload[:type]} for #{duration}s, documents total: #{payload[:import].try(:[], :index).to_i}"
-      payload[:errors].each do |action, errors|
-        puts "    #{action.to_s.humanize} errors:"
-        errors.each do |error, documents|
-          puts "      `#{error}`"
-          puts "        on #{documents.count} documents: #{documents}"
-        end
-      end if payload[:errors]
-    end
-  end
-
-  def eager_load_chewy!
-    dirs = Chewy::Railtie.all_engines.map { |engine| engine.paths['app/chewy'].existent }.flatten.uniq
-
-    dirs.each do |dir|
-      Dir.glob(File.join(dir, '**/*.rb')).each do |file|
-        require_dependency file
+  class << self
+    def subscribe_task_stats!
+      ActiveSupport::Notifications.subscribe('import_objects.chewy') do |name, start, finish, id, payload|
+        duration = (finish - start).round(2)
+        puts "  Imported #{payload[:type]} for #{duration}s, documents total: #{payload[:import].try(:[], :index).to_i}"
+        payload[:errors].each do |action, errors|
+          puts "    #{action.to_s.humanize} errors:"
+          errors.each do |error, documents|
+            puts "      `#{error}`"
+            puts "        on #{documents.count} documents: #{documents}"
+          end
+        end if payload[:errors]
       end
     end
-  end
 
-  def normalize_index index
-    "#{index.to_s.gsub(/index\z/i, '').camelize}Index".constantize
-  end
+    def eager_load_chewy!
+      dirs = Chewy::Railtie.all_engines.map { |engine| engine.paths['app/chewy'].existent }.flatten.uniq
 
-  # Performs zero downtime reindexing of all documents in the specified index.
-  def reset_index index
-    index = normalize_index(index)
-    puts "Resetting #{index}"
-    index.reset! (Time.now.to_f * 1000).round
-  end
-
-  # Performs zero downtime reindexing of all documents across all indices.
-  def reset_all
-    eager_load_chewy!
-    Chewy::Index.descendants.each { |index| reset_index index }
-  end
-
-  def update_index index
-    index = normalize_index(index)
-    puts "Updating #{index}"
-    if index.exists?
-      index.import
-    else
-      puts "Index `#{index.index_name}` does not exists. Use rake chewy:reset[#{index.index_name}] to create and update it."
+      dirs.each do |dir|
+        Dir.glob(File.join(dir, '**/*.rb')).each do |file|
+          require_dependency file
+        end
+      end
     end
-  end
 
-  def update_all
-    eager_load_chewy!
-    Chewy::Index.descendants.each { |index| update_index index }
+    def normalize_index index
+      "#{index.to_s.gsub(/index\z/i, '').camelize}Index".constantize
+    end
+
+    # Performs zero downtime reindexing of all documents in the specified index.
+    def reset_index index
+      index = normalize_index(index)
+      puts "Resetting #{index}"
+      index.reset! (Time.now.to_f * 1000).round
+    end
+
+    # Performs zero downtime reindexing of all documents across all indices.
+    def reset_all
+      eager_load_chewy!
+      Chewy::Index.descendants.each { |index| reset_index index }
+    end
+
+    def update_index index
+      index = normalize_index(index)
+      puts "Updating #{index}"
+      if index.exists?
+        index.import
+      else
+        puts "Index `#{index.index_name}` does not exists. Use rake chewy:reset[#{index.index_name}] to create and update it."
+      end
+    end
+
+    def update_all
+      eager_load_chewy!
+      Chewy::Index.descendants.each { |index| update_index index }
+    end
   end
 end

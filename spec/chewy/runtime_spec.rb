5746ec0e1e304a90411b624a605786e8d2759fe2
Eugene Pirogov <iamexile@gmail.com>
2015-09-22 15:15:15 +0300

    Bump elasticsearch version

--- spec/chewy/runtime_spec.rb
+++ spec/chewy/runtime_spec.rb
@@ -4,6 +4,6 @@ describe Chewy::Runtime do
   describe '.version' do
     specify { expect(described_class.version).to be_a(described_class::Version) }
     specify { expect(described_class.version).to be >= '1.0' }
-    specify { expect(described_class.version).to be < '1.6' }
+    specify { expect(described_class.version).to be < '1.8' }
   end
 end

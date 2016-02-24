0ee3b9de166e2062a2590fa05ae88fa393f50156
Bartek Bułat <bartek.bulat@gmail.com>
2016-02-24 14:04:07 +0100

    Split import_scope into two methods

--- lib/chewy/type/adapter/active_record.rb
+++ lib/chewy/type/adapter/active_record.rb
@@ -22,38 +22,46 @@ module Chewy
           @default_scope = @default_scope.reorder(nil).limit(nil).offset(nil)
         end
 
-        def import_scope(scope, batch_size, sort_by_updated_at)
+        def import_scope(scope, batch_size, sort_by_updated_at, &block)
+          if sort_by_updated_at && timestamp_column_name
+            import_scope_ordered_by_timestamp(scope, batch_size, &block)
+          else
+            import_scope_ordered_by_id(scope, batch_size, &block)
+          end
+        end
+
+        def import_scope_ordered_by_id(scope, batch_size)
           result = true
 
-          if !sort_by_updated_at || timestamp_column_name.nil?
-            scope = scope.reorder(id_column.asc).limit(batch_size)
+          scope = scope.reorder(id_column.asc).limit(batch_size)
 
-            ids = pluck_ids(scope)
+          ids = pluck_ids(scope)
 
-            while ids.present?
-              result &= yield grouped_objects(default_scope_where_ids_in(ids))
-              break if ids.size < batch_size
-              ids = pluck_ids(scope.where(id_column.gt(ids.last)))
-            end
-          else
-            scope = scope.reorder(timestamp_column.asc, id_column.asc).limit(batch_size)
+          while ids.present?
+            result &= yield grouped_objects(default_scope_where_ids_in(ids))
+            break if ids.size < batch_size
+            ids = pluck_ids(scope.where(id_column.gt(ids.last)))
+          end
 
-            ids = pluck_ids_and_dates(scope)
+          result
+        end
 
-            # order by update_at, id
-            #
-            # row.update_at> last_updated_at || row.updated_at = last. && row.id > last_id
+        def import_scope_ordered_by_timestamp(scope, batch_size)
+          result = true
 
-            while ids.present?
-              result &= yield grouped_objects(scope_where_ids_in(scope, ids.map(&:first)))
-              break if ids.size < batch_size
-              last_id, last_updated_at = ids.last
-              ids = pluck_ids_and_dates(
-                scope.where(
-                  timestamp_column.gt(last_updated_at).or( timestamp_column.eq(last_updated_at).and( id_column.gt(last_id) ) )
-                )
+          scope = scope.reorder(timestamp_column.asc, id_column.asc).limit(batch_size)
+
+          ids = pluck_ids_and_dates(scope)
+
+          while ids.present?
+            result &= yield grouped_objects(scope_where_ids_in(scope, ids.map(&:first)))
+            break if ids.size < batch_size
+            last_id, last_updated_at = ids.last
+            ids = pluck_ids_and_dates(
+              scope.where(
+                timestamp_column.gt(last_updated_at).or( timestamp_column.eq(last_updated_at).and( id_column.gt(last_id) ) )
               )
-            end
+            )
           end
 
           result

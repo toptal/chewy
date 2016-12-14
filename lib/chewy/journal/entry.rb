eeb69d88350631852d719fee860cb4451823a043
Sergey Kintsel <sergey.kintsel@toptal.com>
2016-12-14 18:43:21 +0600

    Add specs for Chewy::Journal::Entry

--- lib/chewy/journal/entry.rb
+++ lib/chewy/journal/entry.rb
@@ -1,5 +1,6 @@
 module Chewy
   class Journal
+    # Describes a journal entry and provides necessary assisting methods
     class Entry
       ATTRIBUTES = %w(index_name type_name action object_ids created_at).freeze
 
@@ -11,6 +12,9 @@ module Chewy
         end
       end
 
+      # Loads all entries since some time
+      # @param time [Integer] a timestamp from which we load a journal
+      # @param indices [Array<Chewy::Index>] journal records related to these indices will be loaded only
       def self.since(time, indices = [])
         query = Query.new(time, :gte, indices).to_h
         parameters = { index: Journal.index_name, type: Journal.type_name, body: query }
@@ -24,11 +28,18 @@ module Chewy
         end
       end
 
+      # Groups a list of entries by full type name to decrease
+      # a number of calls to Elasticsearch during journal apply
+      # @param entries [Array<Chewy::Journal::Entry>]
       def self.group(entries)
         entries.group_by(&:full_type_name)
           .map { |_, grouped_entries| grouped_entries.reduce(:merge) }
       end
 
+      # Allows to filter one list of entries from another
+      # If any records with the same full type name are found then their object_ids will be subtracted
+      # @param from [Array<Chewy::Journal::Entry>] from which list we subtract another
+      # @param what [Array<Chewy::Journal::Entry>] what we subtract
       def self.subtract(from, what)
         return from if what.empty?
         from.each do |from_entry|
@@ -39,8 +50,10 @@ module Chewy
         from.delete_if(&:empty?)
       end
 
+      # Get the most recent timestamp from a list of entries
+      # @param entries [Array<Chewy::Journal::Entry>]
       def self.recent_timestamp(entries)
-        entries.map(&:created_at).max
+        entries.map { |entry| entry.created_at.to_i }.max
       end
 
       def index
@@ -59,11 +72,11 @@ module Chewy
       end
 
       def ==(other)
-        full_type_name == other.try!(:full_type_name)
+        full_type_name == other.try(:full_type_name)
       end
 
       def empty?
-        object_ids.empty?
+        !object_ids || object_ids.empty?
       end
     end
   end

7cbfaff88d945b830338b9840a744fd678b8262d
Eric Proulx <eproulx@petalmd.com>
2017-01-12 22:28:51 -0500

    Added condition for refresh with `disable_refresh_async`

--- lib/chewy/strategy/sidekiq.rb
+++ lib/chewy/strategy/sidekiq.rb
@@ -16,7 +16,7 @@ module Chewy
         sidekiq_options queue: :chewy
 
         def perform(type, ids, options = {})
-          options[:refresh] = !Chewy.disable_refresh_async
+          options[:refresh] = !Chewy.disable_refresh_async if Chewy.disable_refresh_async
           type.constantize.import!(ids, options)
         end
       end

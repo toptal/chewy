c2e4dda0efe6962222bf63f7eaa9c4303a2b0674
josephchoe <joseph.choe@jibjab.com>
2017-06-27 10:57:53 -0700

    Add args for shoryuken spec

--- spec/chewy/strategy/shoryuken_spec.rb
+++ spec/chewy/strategy/shoryuken_spec.rb
@@ -32,6 +32,7 @@ if defined?(::Shoryuken)
     specify do
       Chewy.settings[:shoryuken] = {queue: 'low'}
       expect(Chewy::Strategy::Shoryuken::Worker).to receive(:perform_async)
+        .with(hash_including(index: CitiesIndex::City, ids: [city.id, other_city.id]), hash_including(queue: 'low'))
       Chewy.strategy(:shoryuken) do
         [city, other_city].map(&:save!)
       end

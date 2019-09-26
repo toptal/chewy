7ccf5b29f293a10d6b19a43cbe91d0021f60d61e
mpeychich <mpeychich@mac.com>
2019-09-26 11:48:55 -0700

    Add Chewy::MultiSearch Feature
    Adds the ability to issue a single request for multiple search queries
    by leveraging the Elasticsearch Multi Search API.

--- lib/chewy/search/request.rb
+++ lib/chewy/search/request.rb
@@ -112,7 +112,16 @@ module Chewy
       # @see Chewy::Search::Response
       # @return [Chewy::Search::Response] a response object instance
       def response
-        @response ||= Response.new(perform, loader, collection_paginator)
+        @response ||= build_response(perform)
+      end
+
+      # Wraps and sets the raw Elasticsearch response to provide access
+      # to convenience methods.
+      # 
+      # @see Chewy::Search::Response
+      # @param from_elasticsearch [Hash] An Elasticsearch response
+      def response=(from_elasticsearch)
+        @response = build_response(from_elasticsearch)
       end
 
       # ES request body
@@ -948,6 +957,13 @@ module Chewy
           end
       end
 
+      # Returns whether or not the query has been performed.
+      # 
+      # @return [true, false]
+      def performed?
+        !@response.nil?
+      end
+
     protected
 
       def initialize_clone(origin)
@@ -956,6 +972,10 @@ module Chewy
       end
 
     private
+
+      def build_response(raw_response)
+        Response.new(raw_response, loader, collection_paginator)
+      end
 
       def compare_internals(other)
         parameters == other.parameters
@@ -1031,10 +1051,6 @@ module Chewy
         else
           hit.fetch('_source', {})[field]
         end
-      end
-
-      def performed?
-        !@response.nil?
       end
 
       def collection_paginator
---
+++ spec/chewy/multi_search_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+require 'chewy/multi_search'
+
+describe Chewy::MultiSearch do
+  before { Chewy.massacre }
+
+  before do
+    stub_model(:city)
+    stub_model(:country)
+
+    stub_index(:places) do
+      define_type City do
+        field :name, KEYWORD_FIELD
+        field :country_id, KEYWORD_FIELD
+
+        def self.aggregate_by_country
+          aggs(country: { terms: { field: :country_id } })
+        end
+      end
+    end
+  end
+
+  let(:places_query) { PlacesIndex.all }
+
+  describe '#queries' do
+    specify 'returns the queries that are a part of the multi search' do
+      multi_search = described_class.new([places_query])
+      expect(multi_search.queries).to contain_exactly(places_query)
+    end
+  end
+
+  describe '#add_query' do
+    specify 'adds a query to the multi search' do
+      multi_search = described_class.new([])
+      expect {
+        multi_search.add_query(places_query)
+      }.to change {
+        multi_search.queries
+      }.from([]).to([places_query])
+    end
+  end
+
+  context 'when given two queries' do
+    let(:queries) { [aggregates, results] }
+    let(:aggregates) { PlacesIndex::City.aggregate_by_country.limit(0) }
+    let(:results) { PlacesIndex::City.limit(10) }
+    let(:multi_search) { described_class.new(queries) }
+    let(:cities) { Array.new(3) { |i| City.create! name: "Name#{i + 2}", country_id: i + 1 } }
+    before { PlacesIndex.import! city: cities }
+
+    describe '#perform' do
+      specify 'performs each query' do
+        expect { multi_search.perform }.
+          to change(aggregates, :performed?).from(false).to(true).
+          and change(results, :performed?).from(false).to(true)
+      end
+
+      specify 'issues a single request using the msearch endpoint', :aggregate_failures do
+        expect(Chewy.client).to receive(:msearch).once.and_return({'responses' => []})
+        expect(Chewy.client).to_not receive(:search)
+        multi_search.perform
+      end
+    end
+
+    describe '#responses' do
+      subject(:responses) { multi_search.responses }
+
+      context 'on a previously performed multi search' do
+        before { multi_search.perform }
+
+        it 'does not perform the query again' do
+          expect(Chewy.client).to_not receive(:msearch)
+          multi_search.responses
+        end
+      end
+
+      specify 'returns the results of each query', :aggregate_failures do
+        is_expected.to have(2).responses
+        expect(responses[0]).to be_a(Chewy::Search::Response)
+        expect(responses[1]).to be_a(Chewy::Search::Response)
+        expect(responses[1].wrappers).to all(be_a PlacesIndex::City)
+      end
+    end
+  end
+end

8f3f03cda0439f46078f66698e8d819a3e638d66
Maciej Rzasa <maciej.rzasa@toptal.com>
2021-02-12 14:28:46 +0100

    Remove parent param from mapping spec

--- spec/chewy/type/mapping_spec.rb
+++ spec/chewy/type/mapping_spec.rb
@@ -90,12 +90,11 @@ describe Chewy::Type::Mapping do
         before do
           stub_index(:products) do
             define_type :product do
-              root _parent: 'project', other_option: 'nothing' do
+              root other_option: 'nothing' do
                 field :name do
                   field :last_name # will be redefined in the following root flock
                 end
               end
-              root _parent: 'something_else'
               root other_option: 'option_value' do
                 field :identifier
                 field :name, type: 'integer'
@@ -111,39 +110,8 @@ describe Chewy::Type::Mapping do
               identifier: {type: Chewy.default_field_type}
             },
             other_option: 'option_value',
-            _parent: {type: 'something_else'}
           })
         end
-      end
-    end
-
-    context 'parent-child relationship' do
-      context do
-        before do
-          stub_index(:products) do
-            define_type :product do
-              root _parent: 'project', parent_id: -> { project_id } do
-                field :name, 'surname'
-              end
-            end
-          end
-        end
-
-        specify { expect(product.mappings_hash[:product][:_parent]).to eq(type: 'project') }
-      end
-
-      context do
-        before do
-          stub_index(:products) do
-            define_type :product do
-              root parent: {'type' => 'project'}, parent_id: -> { project_id } do
-                field :name, 'surname'
-              end
-            end
-          end
-        end
-
-        specify { expect(product.mappings_hash[:product][:_parent]).to eq('type' => 'project') }
       end
     end
   end

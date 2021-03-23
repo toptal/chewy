6368848e0cf3a1a50528aca8f0a1e05adae305ec
Vitalina-Vakulchyk <vitalina.vakulchyk@toptal.com>
2021-03-23 13:42:48 +0200

    Mongoid: remove MongoidMethods module from lib/chewy/type/observe.rb

--- lib/chewy/type/observe.rb
+++ lib/chewy/type/observe.rb
@@ -47,16 +47,6 @@ module Chewy
 
       extend Helpers
 
-      module MongoidMethods
-        def update_index(type_name, *args, &block)
-          callback_options = Observe.extract_callback_options!(args)
-          update_proc = Observe.update_proc(type_name, *args, &block)
-
-          after_save(callback_options, &update_proc)
-          after_destroy(callback_options, &update_proc)
-        end
-      end
-
       module ActiveRecordMethods
         def update_index(type_name, *args, &block)
           callback_options = Observe.extract_callback_options!(args)

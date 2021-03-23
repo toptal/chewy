b90c3f6ca667ea93b755420c76c2807ee5ad4845
Vitalina-Vakulchyk <vitalina.vakulchyk@toptal.com>
2021-03-23 12:07:36 +0200

    Resque: Remove resque from /lib/chewy/config.rb

--- lib/chewy/config.rb
+++ lib/chewy/config.rb
@@ -29,7 +29,7 @@ module Chewy
                   # Set number_of_replicas to 0 before reset and put the original value after
                   # https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-update-settings.html
                   :reset_no_replicas,
-                  # Refresh or not when import async (sidekiq, resque, activejob)
+                  # Refresh or not when import async (sidekiq, activejob)
                   :disable_refresh_async,
                   # Default options for root of Chewy type. Allows to set default options
                   # for type mappings like `_all`.

415208722e33d3a7934cc19a5bd6af46ff43fa64
Vitalina-Vakulchyk <vitalina.vakulchyk@toptal.com>
2021-05-14 13:02:52 +0300

    Update spec for rspec mock_elasticsearch_response helper

--- lib/chewy/rspec/mock_elasticsearch_response.rb
+++ lib/chewy/rspec/mock_elasticsearch_response.rb
@@ -6,10 +6,12 @@
 #   mock_elasticsearch_response(expected_response_here)
 #   CitiesIndex.client.search(expected_response_here)
 #
-RSpec::Matchers.define :mock_elasticsearch_response do |raw_response = {}|
+RSpec::Matchers.define :mock_elasticsearch_response do |raw_response = {}, &block|
   match do
     mocked_request = instance_double('Chewy::Search::Request', indexes: [])
     allow(Chewy::Search::Request).to receive(:new).and_return(mocked_request)
     allow(mocked_request).to receive(:build_response).and_return(raw_response)
+
+    block.call
   end
 end
--- spec/chewy/rspec/mock_elasticsearch_response_spec.rb
+++ spec/chewy/rspec/mock_elasticsearch_response_spec.rb
@@ -9,7 +9,54 @@ describe :mock_elasticsearch_response do
 
   let(:dummy_query) { {} }
 
-  specify do
+  let(:hits) do
+    [
+      {
+        '_index' => 'dummies',
+        '_type' => '_doc',
+        '_id' => '1',
+        '_score' => 3.14,
+        '_source' => source
+      }
+    ]
+  end
+
+  let(:source) { {'name' => 'some_name'} }
+  let(:sources) { [source] }
+
+  context 'mocks by raw response' do
+    let(:raw_response) do
+      {
+        'took' => 4,
+        'timed_out' => false,
+        '_shards' => {'total' => 1, 'successful' => 1, 'skipped' => 0, 'failed' => 0},
+        'hits' => {
+          'total' => {
+            'value' => 1,
+            'relation' => 'gte'
+          },
+          'max_score' => 0.0005,
+          'hits' => hits
+        }
+      }
+    end
+
+    specify do
+      mock_elasticsearch_response(raw_response) do
+        expect(CitiesIndex.query({}).hits).to eq(hits)
+      end
+    end
+  end
+
+  xcontext 'mocks by response sources' do
+    specify do
+      mock_elasticsearch_response_sources(sources) do
+        expect(CitiesIndex.query({}).hits).to eq(hits)
+      end
+    end
+  end
+
+  xspecify do
     mock_elasticsearch_response(dummy_query)
     CitiesIndex.client.search(dummy_query)
   end

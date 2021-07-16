e2854ed45a9240c1acc51938721465dcf68619d4
Tema Bolshakov <artem.bolshakov@toptal.com>
2021-07-16 13:18:34 +0300

    Use Chewy.thread_local_data to access Thread.current
    Having `Thread.current` access isolated help to extend chewy. After isolating it we can provide dry-effects integration.

--- lib/chewy/runtime.rb
+++ lib/chewy/runtime.rb
@@ -3,7 +3,7 @@ require 'chewy/runtime/version'
 module Chewy
   module Runtime
     def self.version
-      Thread.current[:chewy_runtime_version] ||= Version.new(Chewy.client.info['version']['number'])
+      Chewy.thread_local_data[:chewy_runtime_version] ||= Version.new(Chewy.client.info['version']['number'])
     end
   end
 end

33e40cd001e4e51b199c7c045eead58b30e770e0
Slava Mefodin <slava.mefodin@toptal.com>
2021-12-14 10:14:49 +0000

    Call symbols with send and take procs arity into account

--- lib/chewy/index/observe/callback.rb
+++ lib/chewy/index/observe/callback.rb
@@ -12,17 +12,21 @@ module Chewy
           return if @if_filter && !eval_filter(@if_filter, context)
           return if @unless_filter && eval_filter(@unless_filter, context)
 
-          context.instance_eval(&@executable)
+          eval_proc(@executable, context)
         end
 
       private
 
         def eval_filter(filter, context)
           case filter
-          when Symbol then context.instance_exec(&filter.to_proc)
-          when Proc then context.instance_exec(&filter)
+          when Symbol then context.send(filter)
+          when Proc then eval_proc(filter, context)
           else filter
           end
+        end
+
+        def eval_proc(executable, context)
+          executable.arity.zero? ? context.instance_exec(&executable) : executable.call(context)
         end
       end
     end

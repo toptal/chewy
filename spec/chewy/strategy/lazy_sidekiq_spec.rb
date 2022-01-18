053d85bf00af0392e4fd46f2321358d6795254b6
Slava Mefodin <slava.mefodin@toptal.com>
2022-01-18 14:51:50 +0000

    Update LazySidekiq specs

--- spec/chewy/strategy/lazy_sidekiq_spec.rb
+++ spec/chewy/strategy/lazy_sidekiq_spec.rb
@@ -7,12 +7,12 @@ if defined?(::Sidekiq)
     around do |example|
       sidekiq_settings = Chewy.settings[:sidekiq]
       Chewy.settings[:sidekiq] = {queue: 'low'}
-      Chewy.strategy(:lazy_sidekiq) { example.run }
+      Chewy.strategy(:bypass) { example.run }
       Chewy.settings[:sidekiq] = sidekiq_settings
     end
     before { ::Sidekiq::Worker.clear_all }
 
-    context 'tests from sidekiq_spec + some more' do
+    context 'strategy' do
       before do
         stub_model(:city) do
           update_index('cities') { self }
@@ -26,18 +26,25 @@ if defined?(::Sidekiq)
       let(:city) { City.create!(name: 'hello') }
       let(:other_city) { City.create!(name: 'world') }
 
-      specify do
+      it 'does not update indices synchronously' do
         expect { [city, other_city].map(&:save!) }
           .not_to update_index(CitiesIndex, strategy: :lazy_sidekiq)
       end
 
-      specify do
+      it 'updates indices asynchronously on record save' do
         expect(::Sidekiq::Client).to receive(:push)
-          .with(hash_including('class' => Chewy::Strategy::LazySidekiq::IndicesUpdateWorker, 'queue' => 'low'))
+          .with(hash_including(
+                  'class' => Chewy::Strategy::LazySidekiq::IndicesUpdateWorker,
+                  'queue' => 'low'
+                ))
           .and_call_original
           .once
         expect(::Sidekiq::Client).to receive(:push)
-          .with(hash_including('class' => Chewy::Strategy::Sidekiq::Worker, 'queue' => 'low'))
+          .with(hash_including(
+                  'class' => Chewy::Strategy::Sidekiq::Worker,
+                  'queue' => 'low',
+                  'args' => ['CitiesIndex', [city.id, other_city.id]]
+                ))
           .and_call_original
           .once
         ::Sidekiq::Testing.inline! do
@@ -47,26 +54,58 @@ if defined?(::Sidekiq)
         end
       end
 
-      specify do
-        expect(CitiesIndex).to receive(:import!).with([city.id, other_city.id], suffix: '201601')
-        Chewy::Strategy::Sidekiq::Worker.new.perform('CitiesIndex', [city.id, other_city.id], suffix: '201601')
+      it 'updates indices asynchronously with falling back to sidekiq strategy on record destroy' do
+        expect(::Sidekiq::Client).not_to receive(:push)
+          .with(hash_including(
+                  'class' => Chewy::Strategy::LazySidekiq::IndicesUpdateWorker,
+                  'queue' => 'low'
+                ))
+        expect(::Sidekiq::Client).to receive(:push)
+          .with(hash_including(
+                  'class' => Chewy::Strategy::Sidekiq::Worker,
+                  'queue' => 'low',
+                  'args' => ['CitiesIndex', [city.id, other_city.id]]
+                ))
+          .and_call_original
+          .once
+        ::Sidekiq::Testing.inline! do
+          expect { [city, other_city].map(&:destroy) }.to update_index(CitiesIndex, strategy: :sidekiq)
+        end
       end
 
-      specify do
-        allow(Chewy).to receive(:disable_refresh_async).and_return(true)
-        expect(CitiesIndex).to receive(:import!).with([city.id, other_city.id], suffix: '201601', refresh: false)
-        Chewy::Strategy::Sidekiq::Worker.new.perform('CitiesIndex', [city.id, other_city.id], suffix: '201601')
-      end
-
-      specify do
-        expect(CitiesIndex).to receive(:import!).with([city.id], {})
+      it 'calls Index#import!' do
+        expect(CitiesIndex).to receive(:import!).with([city.id, other_city.id])
         expect(::Sidekiq::Client).to receive(:push)
           .with(hash_including('class' => Chewy::Strategy::Sidekiq::Worker, 'queue' => 'low'))
           .and_call_original
-        allow(City).to receive(:where).with(id: city.id).and_return([city])
+
+        allow(City).to receive(:where).with(id: [city.id, other_city.id]).and_return([city, other_city])
         expect(city).to receive(:run_chewy_callbacks).and_call_original
+        expect(other_city).to receive(:run_chewy_callbacks).and_call_original
+
         ::Sidekiq::Testing.inline! do
-          Chewy::Strategy::LazySidekiq::IndicesUpdateWorker.new.perform({'City' => city.id})
+          Chewy::Strategy::LazySidekiq::IndicesUpdateWorker.new.perform({'City' => [city.id, other_city.id]})
+        end
+      end
+
+      context 'when Chewy.disable_refresh_async is true' do
+        before do
+          allow(Chewy).to receive(:disable_refresh_async).and_return(true)
+        end
+
+        it 'calls Index#import! with refresh false' do
+          expect(CitiesIndex).to receive(:import!).with([city.id, other_city.id], refresh: false)
+          expect(::Sidekiq::Client).to receive(:push)
+            .with(hash_including('class' => Chewy::Strategy::Sidekiq::Worker, 'queue' => 'low'))
+            .and_call_original
+
+          allow(City).to receive(:where).with(id: [city.id, other_city.id]).and_return([city, other_city])
+          expect(city).to receive(:run_chewy_callbacks).and_call_original
+          expect(other_city).to receive(:run_chewy_callbacks).and_call_original
+
+          ::Sidekiq::Testing.inline! do
+            Chewy::Strategy::LazySidekiq::IndicesUpdateWorker.new.perform({'City' => [city.id, other_city.id]})
+          end
         end
       end
     end
@@ -76,32 +115,39 @@ if defined?(::Sidekiq)
 
       let(:update_condition) { true }
 
+      before do
+        city_model
+        country_model
+
+        City.belongs_to :country
+        Country.has_many :cities
+
+        stub_index(:cities) do
+          index_scope City
+        end
+
+        stub_index(:countries) do
+          index_scope Country
+        end
+      end
+
       context 'state dependent' do
-        before do
+        let(:city_model) do
           stub_model(:city) do
             update_index(-> { 'cities' }, :self)
             update_index('countries') { changes['country_id'] || previous_changes['country_id'] || country }
           end
+        end
 
+        let(:country_model) do
           stub_model(:country) do
-            update_index('cities', if: -> { update_condition_state }) { cities }
+            update_index('cities', if: -> { state_dependent_update_condition }) { cities }
             update_index(-> { 'countries' }, :self)
-            attr_accessor :update_condition_state
-          end
-
-          City.belongs_to :country
-          Country.has_many :cities
-
-          stub_index(:cities) do
-            index_scope City
-          end
-
-          stub_index(:countries) do
-            index_scope Country
+            attr_accessor :state_dependent_update_condition
           end
         end
 
-        context do
+        context 'city updates' do
           let!(:country1) { Country.create!(id: 1) }
           let!(:country2) { Country.create!(id: 2) }
           let!(:city) { City.create!(id: 1, country: country1) }
@@ -114,10 +160,10 @@ if defined?(::Sidekiq)
           end
         end
 
-        context do
+        context 'country updates' do
           let!(:country) do
             cities = Array.new(2) { |i| City.create!(id: i) }
-            Country.create!(id: 1, cities: cities, update_condition_state: update_condition)
+            Country.create!(id: 1, cities: cities, state_dependent_update_condition: update_condition)
           end
 
           it 'does not update index because state of attribute cannot be fetched' do
@@ -126,33 +172,26 @@ if defined?(::Sidekiq)
         end
       end
 
-      context do
-        before do
+      context 'state independent' do
+        let(:city_model) do
           stub_model(:city) do
             update_index(-> { 'cities' }, :self)
             update_index('countries') { country }
           end
-
-          stub_model(:country) do
-            update_index('cities', if: -> { update_condition_state }) { cities }
-            update_index(-> { 'countries' }, :self)
-          end
-
-          City.belongs_to :country
-          Country.has_many :cities
-
-          stub_index(:cities) do
-            index_scope City
-          end
-
-          stub_index(:countries) do
-            index_scope Country
-          end
-
-          allow_any_instance_of(Country).to receive(:update_condition_state).and_return(update_condition)
         end
 
-        context do
+        let(:country_model) do
+          stub_model(:country) do
+            update_index('cities', if: -> { state_independent_update_condition }) { cities }
+            update_index(-> { 'countries' }, :self)
+          end
+        end
+
+        before do
+          allow_any_instance_of(Country).to receive(:state_independent_update_condition).and_return(update_condition)
+        end
+
+        context 'when city updates' do
           let!(:country1) { Country.create!(id: 1) }
           let!(:country2) { Country.create!(id: 2) }
           let!(:city) { City.create!(id: 1, country: country1) }
@@ -160,20 +199,14 @@ if defined?(::Sidekiq)
           specify { expect { city.save! }.to update_index('cities', strategy: :lazy_sidekiq).and_reindex(city).only }
           specify { expect { city.save! }.to update_index('countries', strategy: :lazy_sidekiq).and_reindex(country1).only }
 
+          specify { expect { city.destroy }.not_to update_index('cities').and_reindex(city).only }
+          specify { expect { city.destroy }.to update_index('countries', strategy: :sidekiq).and_reindex(country1).only }
+
           specify { expect { city.update!(country: nil) }.to update_index('cities', strategy: :lazy_sidekiq).and_reindex(city).only }
-
           specify { expect { city.update!(country: country2) }.to update_index('cities', strategy: :lazy_sidekiq).and_reindex(city).only }
-
-          # See spec/chewy/strategy/lazy_sidekiq_spec.rb:109
-          skip { expect { city.update!(country: nil) }.to update_index('countries', strategy: :lazy_sidekiq).and_reindex(country1).only }
-          # See spec/chewy/strategy/lazy_sidekiq_spec.rb:112
-          skip do
-            expect { city.update!(country: country2) }
-              .to update_index('countries', strategy: :lazy_sidekiq).and_reindex(country1, country2).only
-          end
         end
 
-        context do
+        context 'when country updates' do
           let!(:country) do
             cities = Array.new(2) { |i| City.create!(id: i) }
             Country.create!(id: 1, cities: cities)
@@ -181,7 +214,10 @@ if defined?(::Sidekiq)
           specify { expect { country.save! }.to update_index('cities', strategy: :lazy_sidekiq).and_reindex(country.cities).only }
           specify { expect { country.save! }.to update_index('countries', strategy: :lazy_sidekiq).and_reindex(country).only }
 
-          context 'conditional update' do
+          specify { expect { country.destroy }.to update_index('cities', strategy: :sidekiq).and_reindex(country.cities).only }
+          specify { expect { country.destroy }.not_to update_index('countries').and_reindex(country).only }
+
+          context 'when update condition is false' do
             let(:update_condition) { false }
             specify { expect { country.save! }.not_to update_index('cities', strategy: :lazy_sidekiq) }
           end
